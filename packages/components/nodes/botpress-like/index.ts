5c7f26f7b971e0146c6f1d473bdc934301c34ba3
vandanachaurasia77-eng <vandanachaurasia77@gmail.com>
2025-09-07 15:06:24 +0530

    Create index.ts

---
+++ packages/components/nodes/botpress-like/index.ts
@@ -0,0 +1,7 @@
+module.exports = {
+  ChoiceNode: require('./ChoiceNode').nodeClass,
+  ImageReplyNode: require('./ImageReplyNode').nodeClass,
+  LocationNode: require('./LocationNode').nodeClass,
+  CaptureFormNode: require('./CaptureFormNode').nodeClass,
+  EmailNode: require('./EmailNode').nodeClass
+}
