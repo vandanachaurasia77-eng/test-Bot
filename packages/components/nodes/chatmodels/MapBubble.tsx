998d342542e1f4876c55f7c9839442f31035b208
vandanachaurasia77-eng <vandanachaurasia77@gmail.com>
2025-09-07 15:19:44 +0530

    Delete packages/components/nodes/chatmodels/MapBubble.tsx

--- packages/components/nodes/chatmodels/MapBubble.tsx
+++
@@ -1,7 +0,0 @@
-export default function MapBubble({ url, title }: { url: string; title?: string }) {
-  return (
-    <a href={url} target="_blank" rel="noreferrer" className="underline">
-      📍 {title || 'Open in Google Maps'}
-    </a>
-  )
-}
