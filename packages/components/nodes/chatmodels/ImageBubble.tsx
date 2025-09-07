d23fcb9f5a721dd4084c86e83635c33b739900ae
vandanachaurasia77-eng <vandanachaurasia77@gmail.com>
2025-09-07 15:19:33 +0530

    Delete packages/components/nodes/chatmodels/ImageBubble.tsx

--- packages/components/nodes/chatmodels/ImageBubble.tsx
+++
@@ -1,8 +0,0 @@
-export default function ImageBubble({ imageUrl, caption }: { imageUrl: string; caption?: string }) {
-  return (
-    <div className="space-y-2">
-      <img src={imageUrl} alt={caption || 'image'} className="max-w-xs rounded" />
-      {caption ? <div className="text-sm opacity-80">{caption}</div> : null}
-    </div>
-  )
-}
