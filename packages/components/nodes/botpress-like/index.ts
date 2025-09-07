1264737b962f8c1010a2573b0de26da10b11326a
vandanachaurasia77-eng <vandanachaurasia77@gmail.com>
2025-09-07 15:23:55 +0530

    Delete packages/components/nodes/botpress-like directory

--- packages/components/nodes/botpress-like/index.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  ChoiceNode: require('./ChoiceNode').nodeClass,
-  ImageReplyNode: require('./ImageReplyNode').nodeClass,
-  LocationNode: require('./LocationNode').nodeClass,
-  CaptureFormNode: require('./CaptureFormNode').nodeClass,
-  EmailNode: require('./EmailNode').nodeClass
-}
